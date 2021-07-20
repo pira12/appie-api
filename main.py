9a6d910e2c2e5b6b534b5c0f31012dac7b690c29
pira99 <piraveen@live.nl>
2021-07-21 00:04:19 +0200

    Automated the login with the use of selenium.

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
-import requests
-from bs4 import BeautifulSoup
 import creds
+from selenium import webdriver
 
-login_url = ('')
-secure_url = ('')
+username = creds.username
+password = creds.password
 
-payload = {
-    'username' : creds.username,
-    'password' : creds.password
-}
+driver = webdriver.Chrome('/home/pira/Documents/Personal/webdriver/chromedriver')
+driver.get('https://euidp.aholddelhaize.com/pkmslogin.form')
 
-with requests.session() as s:
-    pass
+username_textbox = driver.find_element_by_id('uid')
+username_textbox.send_keys(username)
+
+password_textbox = driver.find_element_by_id('password')
+password_textbox.send_keys(password)
+
+login_button = driver.find_element_by_class_name('submitButton')
+login_button.click()
+
+driver.get('https://sam.ahold.com/wrkbrn_jct/etm/etmMenu.jsp?locale=nl_NL')
+
+start_button = driver.find_element_by_xpath("//span[text()='Start']")
+start_button.click()
