baf63de0102561b53c5ee77077d8a77051fa6815
pira99 <piraveen@live.nl>
2021-07-23 00:47:39 +0200

    The basic google API calendar integration is done.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import creds
 from selenium import webdriver
+from pprint import pprint
+from Google import Create_Service, convert_to_RFC_datetime
 
 
 def login_navigate():
@@ -25,8 +27,10 @@ def login_navigate():
 def scrape_data():
     ''' Scrape the current month and return all the shifts in a list.'''
     # Scrape the moth and year
+    months = ['Januari', 'Februari', 'Maart', 'April', 'Mei', 'Juni', 'Juli', 'Augustus', 'September', 'Oktober', 'November', 'December']
     month = driver.find_element_by_class_name('calMonthTitle').text
-    year = driver.find_element_by_class_name('calYearTitle').text
+    month = months.index(month) + 1
+    year = int(driver.find_element_by_class_name('calYearTitle').text)
 
     # Scrape all the shifts
     td_tags = driver.find_elements_by_tag_name('td')
@@ -35,16 +39,102 @@ def scrape_data():
     future_shifts = [x for x in driver.find_elements_by_class_name('calendarCellRegularFuture') if x in td_tags]
     all_shifts = [x.text.split('\n')[0:3] for x in (past_shifts + current_shifts + future_shifts)]
 
+    # Divide the times into start and ending time.
+    hour_adjustment = -2
+    for shift in all_shifts:
+        shift[1] = shift[1].replace('(', '')
+        shift[1] = shift[1].replace(')', '')
+        shift[1] = shift[1].split(' - ')
+        shift[1][0] = convert_to_RFC_datetime(year, month, int(shift[0]), int(shift[1][0].split(':')[0]) + hour_adjustment, int(shift[1][0].split(':')[1]))
+        shift[1][1] = convert_to_RFC_datetime(year, month, int(shift[0]), int(shift[1][1].split(':')[0]) + hour_adjustment, int(shift[1][1].split(':')[1]))
+        shift = shift[1:2]
+
     return all_shifts
 
 
+
+def create_calendar():
+    ''' Creates and inserts a calendar if it does not alreaedy exist.'''
+    request_body = {
+        'summary' : 'AH Werkrooster'
+    }
+
+    calendar_items = service.calendarList().list().execute().get('items')
+    calendar_list = [calendar.get('summary') for calendar in calendar_items]
+
+    if request_body.get('summary') not in calendar_list:
+        service.calendars().insert(body=request_body).execute()
+
+
+def update_calendar(shifts):
+    ''' Update the calender and insert the shifts.'''
+    my_calender_id = get_calendar_id('AH Werkrooster')
+
+    # Insert shift as a event.
+    for shift in shifts:
+        event_request_body = {
+            'start' : {
+                'dateTime' : shift[1][0],
+                'timeZone' : 'Europe/Amsterdam'
+            },
+            'end' : {
+                'dateTime' : shift[1][1],
+                'timeZone' : 'Europe/Amsterdam'
+            },
+            'summary' : 'AH Werk',
+            'description' : shift[2],
+            'colorId' : 5,
+            'status' : 'confirmed',
+            'visibility' : 'private'
+        }
+        send_notification = True
+        send_updates = 'none'
+
+        service.events().insert(calendarId=my_calender_id,
+                                sendNotifications=send_notification,
+                                sendUpdates=send_updates,
+                                body=event_request_body).execute()
+
+
+def clear_calendar():
+    ''' Clear everything on the calendar '''
+    my_calender_id = get_calendar_id('AH Werkrooster')
+
+    events = service.events().list(calendarId=my_calender_id).execute().get('items')
+
+    for event in events:
+        service.events().delete(calendarId=my_calender_id, eventId=event.get('id')).execute()
+
+
+def get_calendar_id(title):
+    ''' Get the calendat id, which is connected with the title'''
+    calendar_items = service.calendarList().list().execute().get('items')
+    my_calender = next(filter(lambda x: title in x['summary'], calendar_items))
+    my_calender_id = my_calender.get('id')
+    return my_calender_id
+
+
 if __name__ == '__main__':
+    # Connecting the google calendar API.
+    CLIENT_SECRET_FILE = 'Client_Secret.json'
+    API_NAME = 'calendar'
+    API_VERSION = 'v3'
+    SCOPES = ['https://www.googleapis.com/auth/calendar']
+    service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
+
+    # Creates calendar if needed.
+    create_calendar()
+
     # Start the chrome driver.
     driver = webdriver.Chrome('/home/pira/Documents/Personal/' +
                             'webdriver/chromedriver')
 
-
+    # First navigate and scrape.
     login_navigate()
-    print(scrape_data())
+    shifts = scrape_data()
+
+    # Clean the calender and update the shifts.
+    clear_calendar()
+    update_calendar(shifts)
 
     driver.close()
